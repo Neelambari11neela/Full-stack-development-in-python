21d4a076d7d58e769e38f6836a28d07aa4c4d94b
Neelambari11neela <sivakumarneelambari@gmail.com>
2026-01-23 11:29:46 +0530

    Add files via upload

---
+++ traffic signal.py
@@ -0,0 +1,12 @@
+print("welcome to traffic signal")
+colour = input("enter the colour:")
+if colour == "red":
+    print("Stop")
+elif colour == "yellow":
+    print("get ready")
+elif colour == "green":
+    print("you can go")
+else:
+    print("Invalid signal")
+
+
